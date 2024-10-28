19cd8216a030472d74e817e8d5ca3dcb086f5990
TseganeshM <mogatsega@gmail.com>
2024-10-29 00:40:04 +0300

    modify index.js

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-//const { onRequest } = require("firebase-functions/v2/https");
-//const logger = require("firebase-functions/logger");
-
-//const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv"); // Load environment variables
 dotenv.config();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
-//const { Message } = require("firebase-functions/v1/pubsub");
 
 const app = express(); //initiailie epress
+
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
 
@@ -18,20 +14,28 @@ app.use(express.json());
 
 // Example route to handle payments
 app.get("/", (req, res) => {
-  //  res.status(200).join({ message: "salahagn" });
-  console.log(`suesss`);
+  res.status(201).join({ message: "success" });
 });
+
 app.post("/payment/create", async (req, res) => {
   const total = req.query.total;
-  if (total >= 0) {
-    console.log("payement", total);
-    res.send(total);
+
+  if (total > 0) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+
+    console.log(paymentIntent);
+    res.status(201).json({
+      clientSecret: paymentIntent.client_secret,
+    });
   } else {
     res.status(403).json({ message: "total > 0" });
   }
 });
-app.listen(5000, (err) => {
+http: app.listen(5000, (err) => {
   if (err) throw err;
-  console.log(`Amazon server running on port:5000, http://127.0.0.1:5000`);
+  console.log("Amazon server running on port:5000, http://127.0.0.1:5000");
 });
 //exports.api = functions.https.onRequest(app);
